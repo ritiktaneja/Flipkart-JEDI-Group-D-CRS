159ad8322bce03e24a3863681741eb3986452fca
Ritik Taneja <ritik.taneja@flipkart.com>
2023-02-02 10:40:00 +0530

    updated mockdb class

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/data/MockDB.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/data/MockDB.java
@@ -9,4 +9,12 @@ import java.util.*;
 
 public class MockDB {
     public static Map<Semester, Map<Student, Set<RegisteredCourse>>> registeredCourses = new HashMap<>();
+    public static List<Student> students = new ArrayList<>();
+    public static List<Semester> semesters = new ArrayList<>();
+    public static List<Course> courses = new ArrayList<>();
+
+
+    
+
+
 }
