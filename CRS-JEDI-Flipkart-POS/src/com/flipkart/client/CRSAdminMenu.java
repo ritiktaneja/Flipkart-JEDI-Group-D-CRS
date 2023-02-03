e3206dd01d00053726f25c613c937331d4e242f0
Viraj <viraj.v@BLRETV-Q76R6TFJ.local>
2023-02-03 10:56:14 +0530

    Integrating Admin Services with Admin Menu and Main menu

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/client/CRSAdminMenu.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/client/CRSAdminMenu.java
@@ -1,41 +1,70 @@
 package com.flipkart.client;
 
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+import com.flipkart.service.AdminOperations;
+
+
+import java.util.List;
 import java.util.Scanner;
 
 public class CRSAdminMenu {
 
+    AdminOperations adminOperations = new AdminOperations();
+    private String adminId;
+
     public void createMenu() {
+        System.out.println("Please add your Admin ID");
+        Scanner scanner = new Scanner(System.in);
+        adminId = scanner.nextLine();
+
         System.out.println("********************* Welcome Admin *********************");
         while (true) {
-            System.out.println("********************** Admin Menu **********************");
+            System.out.println("********************************************************");
+            System.out.println("*****************      Admin Menu      *****************");
+            System.out.println("********************************************************");
             System.out.println("1. Add Course to catalog");
             System.out.println("2. Add Professor");
             System.out.println("3. Add Admin");
             System.out.println("4. Approve Student");
             System.out.println("5. Delete Course from catalog");
-            System.out.println("6. Logout");
+            System.out.println("6. View Added Professors");
+            System.out.println("7. Show Courses in catalog");
+            System.out.println("8. Show registered students");
+            System.out.println("9. Logout");
+            System.out.print("Enter your choice : ");
             int choice;
             Scanner sc = new Scanner(System.in);
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
+                    addCourseToCatalog();
                     break;
                 case 2:
+                    addProfessor();
                     break;
                 case 3:
+                    addAdmin();
                     break;
                 case 4:
+                    approveStudent();
                     break;
                 case 5:
+                    deleteCourseFromCatalog();
                     break;
                 case 6:
+                    showProfessor();
                     break;
                 case 7:
+                    viewCoursesInCatalog();
                     break;
                 case 8:
-                    System.out.println("Exiting . . .");
-                    System.exit(0);
+                    showRegisteredStudents();
                     break;
+                case 9:
+                    System.out.println("Heading to Main Menu . . .");
+                    return;
                 default:
                     System.out.println("Enter a valid input");
                     break;
@@ -43,31 +72,95 @@ public class CRSAdminMenu {
         }
     }
 
-    public void addProfessor() {
-
-    }
-    public void viewPendingAdmission(){
-
-    }
-    public void viewCoursesInCatalog(){
-
-    }
-    public void addAdmin() {
-
+    private void showRegisteredStudents() {
+        List<Student> list = adminOperations.viewStudents();
+        if (list == null) {
+            System.out.println("No Student with pending addmission");
+            return;
+        }
+        for (Student c : list) {
+            System.out.println(c);
+        }
     }
 
-    public void approveStudent() {
+    private void addProfessor() {
+        System.out.print("Enter professor Id ");// using semester as catalog id
+        Scanner sc = new Scanner(System.in);
+        String professorId = sc.nextLine();
+        System.out.print("Enter the ProfessorName : ");
+        sc = new Scanner(System.in);
+        String professorName = sc.nextLine();
+        adminOperations.addProfessor(professorId, professorName);
     }
 
-    public void assignCourseToProfessor() {
+    private void showProfessor() {
+        List<Professor> professors = adminOperations.viewProfessors();
+        if (professors == null) {
+            System.out.println("No Professor Added");
+        }
+        for (Professor p : professors) {
+            System.out.println(p);
+        }
+    }
+
+    private void viewPendingAdmission() {
+        List<Student> list = adminOperations.viewPendingApprovals();
+        if (list == null) {
+            System.out.println("No Student with pending addmission");
+            return;
+        }
+        for (Student c : list) {
+            System.out.println(c);
+        }
+    }
+
+    private void viewCoursesInCatalog() {
+        System.out.print("Enter the Semester for creating catalog  : ");// using semester as catalog id
+        Scanner sc = new Scanner(System.in);
+        String catalogId = sc.nextLine();
+        List<Course> list = adminOperations.viewCourses(catalogId);
+        if (list == null) {
+            System.out.println("No course Exist in catalog");
+            return;
+        }
+        for (Course c : list) {
+            System.out.println(c);
+        }
+    }
+
+    private void addAdmin() {
+
 
     }
 
-    public void addCourseToCatalog() {
-
+    private void approveStudent() {
+        System.out.print("Enter the student Id that is to be approved : ");// using semester as catalog id
+        Scanner sc = new Scanner(System.in);
+        String stdentId = sc.nextLine();
+        adminOperations.approveStudent(stdentId);
     }
 
-    public void deleteCourseFromCatalog() {
+    private void addCourseToCatalog() {
+        System.out.print("Enter the Semester for creating catalog  : ");// using semester as catalog id
+        Scanner sc = new Scanner(System.in);
+        String catalogId = sc.nextLine();
+        System.out.print("Enter the course id  : ");
+        sc = new Scanner(System.in);
+        String courseId = sc.next();
+        System.out.print("Enter the course name  : ");
+        sc = new Scanner(System.in);
+        String courseName = sc.next();
+        adminOperations.addCourse(catalogId, courseId, courseName);
+    }
+
+    private void deleteCourseFromCatalog() {
+        System.out.print("Enter the Semester for deleting catalog from catalog : ");// using semester as catalog id
+        Scanner sc = new Scanner(System.in);
+        String catalogId = sc.nextLine();
+        System.out.print("Enter the course id  : ");
+        sc = new Scanner(System.in);
+        String courseId = sc.next();
+        adminOperations.deleteCourse(catalogId, courseId);
 
     }
 }
--- CRS-JEDI-Flipkart-POS/src/com/flipkart/client/CRSApplication.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/client/CRSApplication.java
@@ -1,77 +1,62 @@
 package com.flipkart.client;
-import java.util.*;  
+
+import java.util.*;
 
 public class CRSApplication {
 
 
     public static void main(String[] args) {
-    	System.out.println("Welcome to Course Registration System! choose the Option given below!");
-    	System.out.println("1. Login");
-    	System.out.println("2. Registration of the Student");
-    	System.out.println("3. Update Password");
-    	System.out.println("4. Logout");
-    	System.out.println("Enter your choice: ");
-    	Scanner sc= new Scanner(System.in);
-    	int choice= sc.nextInt(); 
-    	int iterator;
-    	do { 
-    		iterator = choice;
-	    	switch(choice)
-	    	{
-	    		case 1:
-	    			System.out.println("Enter your Username: ");
-		    		String name= sc.next();
-		    		System.out.println("Enter your Password: ");
-		    		String passwd = sc.next();
-		    		System.out.println("Enter your Role(1: for student, 2: for Admin and 3: for Professor): ");
-		    		int role = sc.nextInt();
-		    		//System.out.println("\nLogged in Succesfully!!");
-		    		if(role==1)
-		    		{
-		    			System.out.println("Welcome to Student Menu: ");
-		    			CRSStudentMenu stuobj = new CRSStudentMenu();
-						stuobj.createMenu();
-		    			break;
-		    		}
-		    		else if(role==2)
-		    		{
-		    			System.out.println("Welcome to Admin Menu: ");
-		    			CRSAdminMenu stuobj = new CRSAdminMenu();
-						stuobj.createMenu();
-		    			break;
-		    		}
-		    		else if(role==3)
-		    		{
-		    			System.out.println("Welcome to Professor Menu: ");
-		    			CRSProfessorMenu stuobj = new CRSProfessorMenu();
-						stuobj.createMenu();
-		    			break;
-		    		}
-		    		else
-		    		{
-		    			System.out.println("Invalid Input");
-		    			System.exit(0);
-		    		}
-		    		//break;
-		    		
-	    		case 2:
-	    			System.out.println("Enter details for registration: ");
-	    			break;
-	    			
-	    		case 3:
-	    			System.out.println("Enter details to update password: ");
-	    			break;
-	    			
-	    		case 4:
-	    			System.exit(0);
-	    			break;
-	    			
-	    		default:
-	    			System.out.println("Invlaid Input!! Enter Again: ");
-	    			choice= sc.nextInt();
-	    	}
-    	}while(!((iterator>=1) && (iterator<=4)));
-    System.out.println("Registration Completed!!");
+        System.out.println("Welcome to Course Registration System! choose the Option given below!");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("********************************************************");
+            System.out.println("*****************      Main Menu       *****************");
+            System.out.println("********************************************************");
+            System.out.println("1. Login");
+            System.out.println("2. Registration of the Student");
+            System.out.println("3. Update Password");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter your Username: ");
+                    String name = sc.next();
+                    System.out.println("Enter your Password: ");
+                    String passwd = sc.next();
+                    System.out.println("Enter your Role(1: for student, 2: for Admin and 3: for Professor): ");
+                    int role = sc.nextInt();
+                    if (role == 1) {
+                        System.out.println("Welcome to Student Menu: ");
+                        CRSStudentMenu stuobj = new CRSStudentMenu();
+                        stuobj.createMenu();
+                    } else if (role == 2) {
+                        System.out.println("Welcome to Admin Menu: ");
+                        CRSAdminMenu stuobj = new CRSAdminMenu();
+                        stuobj.createMenu();
+                    } else if (role == 3) {
+                        System.out.println("Welcome to Professor Menu: ");
+                        CRSProfessorMenu stuobj = new CRSProfessorMenu();
+                        stuobj.createMenu();
+                    } else {
+                        System.out.println("Invalid Input");
+                        System.exit(0);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter details for registration: ");
+                    break;
+                case 3:
+                    System.out.println("Enter details to update password: ");
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invlaid Input!! Enter Again: ");
+                    break;
+            }
+        }
     }
 
 //    public void createMainMenu() {
@@ -91,3 +76,4 @@ public class CRSApplication {
 //    }
 
 }
+
